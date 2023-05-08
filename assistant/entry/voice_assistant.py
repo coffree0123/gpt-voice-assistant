d64b9d05f4ae50a1f887419ec0a6c34f8c14ac05
coffree <02320013apis@gmail.com>
2023-05-08 16:01:59 +0800

    Reorganize main code and split different part together.

--- assistant/entry/voice_assistant.py
+++ assistant/entry/voice_assistant.py
@@ -2,52 +2,65 @@ import time
 import pyttsx3
 import speech_recognition as sr
 from dotenv import load_dotenv
-from langchain import OpenAI, LLMChain
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from assistant.prompt.conversation import load_assistant_prompt
 
 load_dotenv()  # take environment variables from .env.
 
 
-def listen():
+def text_from_speech(recognizer, source):
+    print('listening now...')
+    try:
+        audio = recognizer.listen(source, timeout=8, phrase_time_limit=6)
+        print('Recognizing...')
+        text = recognizer.recognize_google(audio)
+    except Exception as e:
+        unrecognized_speech_text = f'Sorry, I didn\'t catch that. Exception was: {e}s'
+        text = unrecognized_speech_text
+
+    return text
+
+
+def build_gpt_agent(prompt: str = 'en_assistant'):
     # Prepare prompt for chatgpt agent. You can add any prompt templete as you wish.
     # Chek prompts folder for more information.
-    prompt = load_assistant_prompt('en_assistant')
+    prompt = load_assistant_prompt(prompt)
 
     # Prepare agent chain for ai-assistant.
-    chatgpt_chain = LLMChain(
-        llm=OpenAI(temperature=0),
+    agent = LLMChain(
+        # You can change model to gpt-4 as well.
+        llm=ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo'),
         prompt=prompt,
         verbose=True,
         memory=ConversationBufferWindowMemory(k=2),
     )
 
-    r = sr.Recognizer()
-    engine = pyttsx3.init()
+    return agent
+
+
+def listen():
+    agent = build_gpt_agent()
+    # Prepare text-to-speech(TTS) model and ASR model.
+    tts = pyttsx3.init()
+    recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-
-        print('Okay, go...')
+        recognizer.adjust_for_ambient_noise(source)
         while (True):
-            text = ''
-            print('listening now...')
-            try:
-                audio = r.listen(source, timeout=8, phrase_time_limit=6)
-                print('Recognizing...')
-                text = r.recognize_google(audio)
-            except Exception as e:
-                unrecognized_speech_text = f'Sorry, I didn\'t catch that. Exception was: {e}s'
-                text = unrecognized_speech_text
+            # Step 1: Speech to text.
+            text = text_from_speech(recognizer, source)
 
-            response_text = chatgpt_chain.predict(human_input=text)
+            # Step 2: Chatgpt response consider input text.
+            response_text = agent.run(human_input=text)
 
             print(f"User: {text}")
             print(f"Response: {response_text}")
 
+            # Step 3: Text to speech.
             time.sleep(1)
-
-            engine.say(response_text)
-            engine.runAndWait()
+            tts.say(response_text)
+            tts.runAndWait()
 
 
 if __name__ == '__main__':
