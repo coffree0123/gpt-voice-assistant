0cb22ec7604f8b54e0fdc24dff30b226a258ed33
黃昱翔 <coffree@huangyuxiangs-Laptop.local>
2023-05-26 21:34:55 +0800

    add nlp agent

--- assistant/entry/nlp_assistant.py
+++ assistant/entry/nlp_assistant.py
@@ -1,28 +1,7 @@
-import time
 from dotenv import load_dotenv
-from langchain import LLMChain
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferWindowMemory
-from assistant.prompt.conversation import load_assistant_prompt
+from assistant.agent import build_gpt_agent
 
 load_dotenv()  # take environment variables from .env.
-
-
-def build_gpt_agent(prompt: str = 'en_assistant'):
-    # Prepare prompt for chatgpt agent. You can add any prompt templete as you wish.
-    # Chek prompts folder for more information.
-    prompt = load_assistant_prompt(prompt)
-
-    # Prepare agent chain for ai-assistant.
-    agent = LLMChain(
-        # You can change model to gpt-4 as well.
-        llm=ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo'),
-        prompt=prompt,
-        verbose=True,
-        memory=ConversationBufferWindowMemory(k=2),
-    )
-
-    return agent
 
 
 def listen():
